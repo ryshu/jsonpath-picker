9fd5d38da92b0168cfff0df98df9660fec492ec2
Oscar MARIE--TAILLEFER <Oscar.Marie-Taillefer@genesys.com>
2019-06-21 11:38:49 +0200

    Alpha-num will not work as uuid selector: [issue](https://stackoverflow.com/questions/20306204/using-queryselector-with-ids-that-are-numbers)

--- src/jsonpath-picker.js
+++ src/jsonpath-picker.js
@@ -337,10 +337,12 @@ function PickEventListener(event) {
 
 // Uniq id generator
 function uuidv4() {
-  return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
-    return v.toString(16);
-  });
+  function randomString(length, chars) {
+      var result = '';
+      for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
+      return result;
+  }
+  return randomString(32, 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
 }
 
 let targetList = [];
